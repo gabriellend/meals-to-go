38e11564d8799e2b812234d2e762822cbd42dfd3
gabriellend <gdvdsn@gmail.com>
2023-04-28 13:42:58 -1000

    Add Loader

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -18,22 +18,20 @@ import {
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Some Restaurant",
-    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
-    photos = [
-      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
-    ],
-    address = "100 some random street",
-    isOpenNow = true,
-    rating = 4,
-    isClosedTemporarily = false,
+    name,
+    icon,
+    photos,
+    address,
+    isOpenNow,
+    rating,
+    isClosedTemporarily,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={5} key={name}>
-      <RestaurantCardCover source={{ uri: photos[0] }} key={name} />
+      <RestaurantCardCover source={{ uri: photos[0] }} />
       <Info>
         <LeftSide>
           <Text variant="label">{name}</Text>
--- src/features/restaurants/screens/restaurants-screen.js
+++ src/features/restaurants/screens/restaurants-screen.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
-import { Searchbar } from "react-native-paper";
+import { Searchbar, ActivityIndicator } from "react-native-paper";
 
 import { Spacer } from "src/components/spacer/spacer.component";
 import { SafeArea } from "src/components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { RestaurantsContext } from "src/services/restaurants/restaurants.context";
+import { colors } from "src/infrastructure/theme/colors";
 
 const SearchView = styled.View`
   padding: ${(props) => props.theme.space.size_16};
@@ -17,25 +18,39 @@ const RestaurantFlatList = styled.FlatList.attrs({
   },
 })``;
 
+const Loader = styled(ActivityIndicator)`
+  flex: 1;
+`;
+
 export const RestaurantsScreen = () => {
   const { restaurants, isLoading, error } = useContext(RestaurantsContext);
 
   return (
     <SafeArea>
-      <SearchView>
-        <Searchbar placeholder="search" />
-      </SearchView>
-      <RestaurantFlatList
-        data={restaurants}
-        renderItem={({ item }) => {
-          return (
-            <Spacer position="bottom" size="large">
-              <RestaurantInfoCard restaurant={item} />
-            </Spacer>
-          );
-        }}
-        keyExtractor={(item) => item.name}
-      />
+      {isLoading ? (
+        <Loader
+          animating={isLoading}
+          color={colors.brand.primary}
+          size="large"
+        />
+      ) : (
+        <>
+          <SearchView>
+            <Searchbar placeholder="search" />
+          </SearchView>
+          <RestaurantFlatList
+            data={restaurants}
+            renderItem={({ item }) => {
+              return (
+                <Spacer position="bottom" size="large">
+                  <RestaurantInfoCard restaurant={item} />
+                </Spacer>
+              );
+            }}
+            keyExtractor={(item) => item.name}
+          />
+        </>
+      )}
     </SafeArea>
   );
 };
